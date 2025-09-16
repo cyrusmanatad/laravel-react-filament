f485ee1253f6a0b4409c957db5696fed35a400f5
Cyrus <c_camanatad@unilab.com.ph>
2025-09-16 21:27:33 +0800

    use guarded property instead of fillable

--- app/Models/BrandManager.php
+++ app/Models/BrandManager.php
@@ -8,18 +8,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class BrandManager extends Model
 {
     use SoftDeletes;
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'site_user_id',
-        'cust_code',
-        'bm_email',
-        'bm_code',
-        'bm_name',
-        'bp_code',
-        'bp_name'
-    ];
+
+    protected array $guarded = [];
 }
